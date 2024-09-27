90fba64e5ba8eef72ade1308ed81badb3c77bdc7
marciosobel <marcinhosobel@hotmail.com>
2024-09-27 11:47:15 -0300

    :sprakles: feat: post person

--- src/main/java/com/backendframeworks/university/controllers/PersonController.java
+++ src/main/java/com/backendframeworks/university/controllers/PersonController.java
@@ -1,8 +1,15 @@
 package com.backendframeworks.university.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.backendframeworks.university.dtos.PersonDto;
+import com.backendframeworks.university.models.Person;
 import com.backendframeworks.university.repositories.PersonRepository;
 
 @RestController
@@ -10,4 +17,13 @@ public class PersonController {
 
 	@Autowired
 	private PersonRepository personRepository;
+
+	@PostMapping("/person")
+	public ResponseEntity<Person> createPerson(@RequestBody PersonDto personDto) {
+		Person person = new Person();
+		BeanUtils.copyProperties(personDto, person);
+
+		Person savedPerson = personRepository.save(person);
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedPerson);
+	}
 }
