68bddb741c4d7180fa0e23b20315e5c0cfbfed46
marciosobel <marcinhosobel@hotmail.com>
2024-09-27 11:54:32 -0300

    :sparkles: feat: Get person

--- src/main/java/com/backendframeworks/university/controllers/PersonController.java
+++ src/main/java/com/backendframeworks/university/controllers/PersonController.java
@@ -1,9 +1,14 @@
 package com.backendframeworks.university.controllers;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +30,14 @@ public class PersonController {
 
 		Person savedPerson = personRepository.save(person);
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedPerson);
+	}
+
+	@GetMapping("/person/{id}")
+	public ResponseEntity<Object> getPersonById(@PathVariable UUID id) {
+		Optional<Person> foundPerson = personRepository.findById(id);
+		if (foundPerson.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person not found");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(foundPerson.get());
 	}
 }
